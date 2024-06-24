d3dd590e688d3a4193a50dc2411713482d80c1d7
LayWasTaken <clarencetoledo08@gmail.com>
2024-06-24 20:47:41 +0800

    Higher Stomp Hight

--- src/Lay/BossFight/entity/attacks/custom/Stomp.php
+++ src/Lay/BossFight/entity/attacks/custom/Stomp.php
@@ -6,7 +6,6 @@ use Lay\BossFight\entity\attacks\AOEAttack;
 use pocketmine\entity\Entity;
 use pocketmine\entity\Living;
 use pocketmine\math\AxisAlignedBB;
-use pocketmine\player\Player;
 
 final class Stomp extends AOEAttack {
 
@@ -20,7 +19,7 @@ final class Stomp extends AOEAttack {
         foreach ($this->source->getWorld()->getNearbyEntities($area) as $entity) {
             if($entity->getId() == $id) continue;
             if(!$entity instanceof Living) continue;
-            $entity->setMotion($entity->getMotion()->add(0, 0.2, 0));
+            $entity->setMotion($entity->getMotion()->add(0, 1, 0));
             $entity->jump();
             $this->baseAttack($entity, $this->damage);
         }
